496e4163c1250f2cc40ef5fd6a06bd36fc7d8a76
Chun-Hung Lin <billy0210828@gmail.com>
2019-03-28 10:15:40 -0400

    Add setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,38 @@
 #!/bin/bash
 # My first script
 
+echo "Step1. Setup python virtual env and activate"
+echo "Step2. Install all the python dependencies"
+echo "Step3. Download web driver"
+echo "Step4. Move web driver to PATH of python virtual env"
+echo "Step5. Run web scraping python script"
+
+
+echo "Enter c if using Google Chrome. Enter f if using Firefox"
+read browser
+
+if [ $browser == 'c' ];
+then	
+	echo "Download chromedriver"
+elif [ $browser == 'f' ];
+then
+	echo "Download geckodriver"
+else
+	echo "Please type in 'c' or 'f'"
+fi
+
+: '
+echo "Step2. Setup python virtual env"
 if [ -d "$env" ]
 then
     echo "Set up a python virtual environment"
 else
 	echo "python virtual environment is already in env/"
 fi
+'
 
 echo "Finish setup in your linux system"
+
+#===========================================
+#Note
+#multi-line comment  : ' ABCD '
